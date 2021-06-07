0c850b42a252a7f6a1cab3724794b88940f10cb7
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-06 21:27:42 -0500

    feat: agregar dashboard

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import logo from '../../logo.svg';
-import '../../App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import Dashboard from '../../components/dashboard/Dashboard';
 
 function App() {
+    const themeMui = createMuiTheme({
+        palette: {
+            primary: {
+                light: '#8e8e8e',
+                main: '#616161',
+                dark: '#373737',
+                contrastText: '#ffffff',
+            },
+        },
+    });
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <MuiThemeProvider theme={themeMui}>
+            <Dashboard />
+        </MuiThemeProvider>
     );
 }
 
